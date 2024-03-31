5eb03a6fa45653ee84efee3a4c4033771a8db8b6
psmith150 <patricksmith150@gmail.com>
2024-03-30 22:25:25 -0500

    Added dialog for setting HP visibility

--- scripts/hide_party_hp.js
+++ scripts/hide_party_hp.js
@@ -1,8 +1,18 @@
 class HidePartyHp {
+    static ID = 'hide-party-hp';
+
+    static FLAGS = {
+        HPCONFIG: 'hp-config'
+    }
+
+    static TEMPLATES = {
+        HPCONFIG: `modules/${this.ID}/templates/hp-config.hbs`
+    }
+
     static init() {
-        game.settings.register('hide-party-hp', 'hide-pcs', {
-			name: game.i18n.localize('hide-party-hp.settings.hide-pcs.name'),
-			hint: game.i18n.localize('hide-party-hp.settings.hide-pcs.hint'),
+        game.settings.register(this.ID, 'hide-pcs', {
+			name: game.i18n.localize(`${this.ID}.settings.hide-pcs.name`),
+			hint: game.i18n.localize(`${this.ID}.settings.hide-pcs.hint`),
 			scope: 'world',
 			config: true,
 			restricted: true,
@@ -32,7 +42,7 @@ class HidePartyHp {
     }
 
     static _shouldHideActor(actor) {
-        return game.settings.get('hide-party-hp', 'hide-pcs') && !actor.testUserPermission(game.user, "OBSERVER");
+        return game.settings.get(this.ID, 'hide-pcs') && !actor.testUserPermission(game.user, "OBSERVER");
     }
 
     static _userIsGm() {
@@ -44,19 +54,58 @@ class HidePartyHp {
             return;
         }
         buttons.unshift({
-            label: game.i18n.localize('hide-party-hp.configure-hp.name'),
+            label: game.i18n.localize(`${this.ID}.configure-hp.name`),
             class: "hide-party-hp-configure",
             icon: "fas fa-heart",
-            onclick: ev => this._onConfigureHp(ev)
+            onclick: ev => this._onConfigureHp(ev, group)
         });
     }
 
-    static _onConfigureHp(event) {
-        console.log("Configuring hp.");
-        const renderOoptions = {
-            left: Math.max(this.position.left, 10),
-            top: Math.max(this.position.top + 20, 10)
+    static updateHpFlags(group, data) {
+        group.actor.setFlag(this.ID, this.FLAGS.HPCONFIG, data)
+    }
+
+    static _onConfigureHp(event, group) {
+        // const renderOptions = {
+        //     left: Math.max(this.position.left, 10),
+        //     top: Math.max(this.position.top + 20, 10)
+        // };
+        new HpConfig(group).render(true);
+    }
+}
+
+class HpConfig extends FormApplication {
+    constructor(group) {
+        super();
+        this.group = group;
+    }
+
+    static get defaultOptions() {
+        const defaults = super.defaultOptions;
+
+        const overrides = {
+            height: 'auto',
+            resizable: true,
+            id: 'party-hp-config',
+            template: HidePartyHp.TEMPLATES.HPCONFIG,
+            title: game.i18n.localize(`${HidePartyHp.ID}.configure-hp-dialog.title`),
+            closeOnSubmit: true,
         };
+
+        const mergedOptions = foundry.utils.mergeObject(defaults, overrides);
+        return mergedOptions;
+    }
+
+    getData(options) {
+        return {
+            members: this.group.object.system.members
+        };
+    }
+
+    async _updateObject(event, formData) {
+        console.log('Data updated');
+        const expandedData = foundry.utils.expandObject(formData);
+        await HidePartyHp.updateHpFlags(this.group, expandedData);
     }
 }
 
@@ -70,4 +119,4 @@ Hooks.on('renderGroupActorSheet', (group, html, groupData) => {
 
 Hooks.on('getGroupActorSheetHeaderButtons', (group, buttons) => {
     HidePartyHp.configureHeaderButtons(group, buttons);
-})
+});
